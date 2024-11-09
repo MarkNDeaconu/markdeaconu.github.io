cf219060f836d1f8fbbd10a35293fab578c081e8
Mark Deaconu <markdeaconu@Marks-MacBook-Pro.local>
2024-11-09 00:13:33 -0500

    hello website

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.page}>
+      Mark Deaconu About
+    </div>
+  );
+}
