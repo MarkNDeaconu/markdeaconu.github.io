ba69d933f36b482744e641e0efdd0b796c45bea0
markndeaconu <mark.deaconu@gmail.com>
2024-11-15 02:33:20 -0500

    stock graphs and youtube videos with iframe

--- app/posts/post1/page.js
+++ app/posts/post1/page.js
@@ -43,8 +43,31 @@ export default function BlogPost() {
   return (
     <div className="blog-post">
       <MarkdownRenderer content={blogContent} />
+      {/* Add the TradingView chart at the bottom */}
+      <div style={{ marginTop: '30px', border: '1px solid #444', borderRadius: '10px', overflow: 'hidden' }}>
+        <iframe
+          src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_abc&symbol=AAPL&interval=D&theme=dark"
+          width="100%"
+          height="400"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+      {/* Add the YouTube video below the stock chart */}
+      <div style={{ marginTop: '30px', textAlign: 'center' }}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Nt6NazoRHOY" 
+          title="YouTube video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
+
 
 
